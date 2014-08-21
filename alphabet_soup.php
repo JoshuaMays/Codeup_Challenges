f1fbb40361a89e950ef30a99fd3348059de59177
haydogsup <joshua.r.mays@gmail.com>
2014-08-21 16:49:28 -0500

    finished alphabet soup.

---
+++ alphabet_soup.php
@@ -0,0 +1,42 @@
+<?php 
+
+function alphabetSoup($str) {
+	// convert string to array
+	$arrayFromString = explode(" ", $str);
+
+	// Supply empty array to hold alphabetized words
+	$savingArray = [];
+	foreach ($arrayFromString as $words) {
+		$workingArray = [];
+
+		// Grab first word in the array to work with it.
+		$grabWord = array_shift($arrayFromString);
+
+		// Convert word into array of its letters
+		$wordArray = str_split($grabWord);
+
+		// Alphabetically sort the letters
+		asort($wordArray);
+
+		// Convert sorted letter array into a string holding the "word"
+		$alphebatizedWord = implode($wordArray);
+
+		// Add the alphabetized word to the saving array. 
+		array_push($savingArray, $alphebatizedWord);
+
+	}
+	// Convert alphabetized word array back into a string.
+	$alphabetizedString = implode(" ", $savingArray);
+
+	return $alphabetizedString;
+}
+
+$str = 'hello world';
+
+$str = alphabetSoup($str);
+
+echo $str . PHP_EOL;
+
+
+
+?>
