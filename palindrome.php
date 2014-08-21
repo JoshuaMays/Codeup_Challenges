fe6485c52e958a6522c78cb58864c25213ab0dcc
haydogsup <joshua.r.mays@gmail.com>
2014-08-21 17:24:58 -0500

    finished palindrome check, need to add feature to ignore punctuation.

--- palindrome.php
+++ palindrome.php
@@ -1,5 +1,25 @@
 <?php
 
+function is_palindrome($string) {
+	// Remove spaces from string.
+	$string = str_replace(' ', '', $string);
+	// Duplicate the string to compare the reversed version.
+	$comparisonString = $string;
+	// Reverse the letters in the string.
+	$reverseString = strrev($comparisonString);
 
+	$compare = strcasecmp($string, $reverseString);
+
+	$amIPalindrome = $compare == 0 ? true : false;
+
+	return $amIPalindrome;
+
+}
+
+$string = "A man a plan a canal panama";
+
+$amIPalindrome = is_palindrome($string);
+
+echo $amIPalindrome . PHP_EOL;
 
 ?>
