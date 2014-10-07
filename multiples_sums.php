8c81b8e5f20a936bfd16fa8df944644ae8cd49fd
haydogsup <joshua.r.mays@gmail.com>
2014-10-06 19:15:45 -0500

    fixed a comment

--- multiples_sums.php
+++ multiples_sums.php
@@ -3,7 +3,7 @@
 $multiples = [];
 $sum = 0;
 
-// Check if the numbers between 1 and 999 are multiples of 3 or 5
+// Check if the numbers between 1 and 1000 are multiples of 3 or 5
 for ($i = 1; $i < 1000; $i++) {
     if (($i % 3 == 0) || ($i % 5 == 0)) {
         // save found multiples to array
